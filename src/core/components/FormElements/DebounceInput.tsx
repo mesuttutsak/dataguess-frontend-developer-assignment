cad05e8d4ea4a493734f6dc0de3d95c92c19f044
Mesut Tutsak <ttsk.mesut@gmail.com>
2023-09-14 03:37:09 +0300

    Debounce input error was fixed.
    It performed as many searches for each letter typed.

--- src/core/components/FormElements/DebounceInput.tsx
+++ src/core/components/FormElements/DebounceInput.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface DebounceInputProps {
     value?: string | undefined;
@@ -9,32 +9,30 @@ interface DebounceInputProps {
     setLoading: (state: boolean) => void;
 }
 
-const DebounceInput: React.FC<DebounceInputProps> = ({ value: initialValue, debounce = 750, placeholder = "", type = 'text', onInputValue, setLoading }: DebounceInputProps) => {
-    const [value, setValue] = useState<string | undefined>(initialValue)
-
-    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value);
-    };
-
-    useEffect(() => {
-        function runTimeout(value:string) {
-            const timeout = setTimeout(() => {
-                if (onInputValue) {
-                    onInputValue(value);
-                    setLoading(true);
-                }
-            }, debounce)
-
-            return () => clearTimeout(timeout);
-        }
-
-        if (typeof value == 'string') runTimeout(value);
-
-    }, [value])
-
-    return (
-        <input defaultValue={initialValue} type={type} placeholder={placeholder} onInput={(e: React.ChangeEvent<HTMLInputElement>) => handleInput(e)} />
-    )
+interface DebounceInputProps {
+    onInputValue: (value: string) => void;
 }
 
-export default DebounceInput
+const DebounceInput: React.FC<DebounceInputProps> = ( { value: initialValue, debounce = 750, placeholder = "", type = 'text', onInputValue, setLoading }: DebounceInputProps ) => {
+    const [inputValue, setInputValue] = useState<string>('');
+
+    useEffect(() => {
+        let debounceTimeout: NodeJS.Timeout;
+        setLoading(true);
+
+        debounceTimeout = setTimeout(() => {
+            onInputValue(inputValue);
+            setLoading(false);
+        }, debounce);
+
+        return () => {
+            clearTimeout(debounceTimeout);
+        };
+    }, [inputValue]);
+
+    return (
+        <input defaultValue={initialValue} type={type} placeholder={placeholder} onInput={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)} />
+    );
+};
+
+export default DebounceInput;
